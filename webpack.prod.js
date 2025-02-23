41b57dac5ab3fcccff5f2027d4ecb6dd765f2e8e
Mariam Hasanat <hasanattmariam@gmail.com>
2025-02-23 23:13:13 +0200

    setup the project, server side and webpack

---
+++ webpack.prod.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+    entry: './client/index.js',
+    mode: 'production',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./client/views/index.html",
+            filename: "./index.html",
+        }),
+        new WorkboxPlugin.GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true,
+            runtimeCaching: [{
+                urlPattern: /\/api\/.*$/,
+                handler: 'NetworkFirst'
+            }, {
+                urlPattern: /\.(?:css|js|html|png|jpg|jpeg|svg)$/,
+                handler: 'CacheFirst',
+                options: {
+                    cacheName: 'assets-cache',
+                    expiration: {
+                        maxEntries: 50,
+                        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                    },
+                },
+            }],
+        }),
+    ],
+    devServer: {
+        port: 6060,
+        allowedHosts: 'all'
+    }
+}
