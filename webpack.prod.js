2ae4a29ca09fa7c1a079eda11f7a35af91d59e5b
Mariam Hasanat <hasanattmariam@gmail.com>
2025-03-02 02:43:40 +0200

    feat: save the name of assets and images after bundling

--- webpack.prod.js
+++ webpack.prod.js
@@ -26,6 +26,9 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
+                generator: {
+                    filename: 'assets/[name][ext]'
+                }
             },
             {
                 test: /\.scss$/,
