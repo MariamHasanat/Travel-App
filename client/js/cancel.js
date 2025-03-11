870d4c2a9308e348082799ee1268d3609c75044f
Mariam Hasanat <hasanattmariam@gmail.com>
2025-03-11 06:10:08 +0200

    fix: cancel button fix the issue of double click for close the form and cancel the operation

--- client/js/cancel.js
+++ client/js/cancel.js
@@ -1,17 +1,12 @@
 function cancel(event) {
     event.preventDefault();
-    document.querySelectorAll("button").forEach(button => {
-        if (button.textContent.trim() === "Cancel") {
-            button.addEventListener("click", (event) => {
-                event.preventDefault();
-                const form = button.closest("form");
-                if (form) {
-                    form.reset();
-                }
-                button.closest(".container").classList.add("display-0");
-            });
-        }
-    });
+    
+    const form = event.target.closest("form");
+    if (form) {
+        form.reset();
+    }
+
+    event.target.closest(".container").classList.add("display-0");
 }
 
 export { cancel };
